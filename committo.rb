c1307b4c3c3786e4e3d5cb783c936a20c202d65b
cybaek <vovoboss@gmail.com>
2025-07-06 03:10:12 +0000

    committo v0.0.4 formula update

--- committo.rb
+++ committo.rb
@@ -1,8 +1,8 @@
 class Committo < Formula
   desc "Commit message generator"
   homepage "https://github.com/dizzyplay/committo"
-  url "https://github.com/dizzyplay/committo/releases/download/v0.0.1/committo-v0.0.1-macos.tar.gz"
-  sha256 "dc4a12fbf9b161c47611f38aa5dd3b97a3311bf3b82e3bfca29f53dd69370049"
+  url "https://github.com/dizzyplay/committo/releases/download/v0.0.4/committo-v0.0.4-macos.tar.gz"
+  sha256 "b1312593ff0446d097a3b329d2f03b26cd42625ba299e9b65849ec300b805788"
 
   def install
     bin.install "committo"
